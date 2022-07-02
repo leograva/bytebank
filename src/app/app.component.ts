4ff2ad58f0f5a1052269b6b927581fa44b255f3b
Leonardo Grava <leonardo.grava@outlook.com>
2022-07-02 02:23:57 -0300

    importing project to github

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bytebank';
+  destino: number;
+  valor: number;
+
+  transferencia: any;
+
+  transferir($event){
+    this.transferencia = $event;
+  }
+
+}
